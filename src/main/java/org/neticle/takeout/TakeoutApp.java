ccb4b90b3272dadd469ead9250238c7f0cb47bfe
neticle2012 <88541227+neticle2012@users.noreply.github.com>
2022-08-05 10:52:15 +0800

    1.3 环境搭建

---
+++ src/main/java/org/neticle/takeout/TakeoutApp.java
@@ -0,0 +1,18 @@
+package org.neticle.takeout;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Faruku123
+ * @version 1.0
+ */
+@Slf4j
+@SpringBootApplication
+public class TakeoutApp {
+    public static void main(String[] args) {
+        SpringApplication.run(TakeoutApp.class, args);
+        log.info("项目启动成功...");
+    }
+}
---
+++ src/main/java/org/neticle/takeout/config/WebMvcConfig.java
@@ -0,0 +1,21 @@
+package org.neticle.takeout.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author Faruku123
+ * @version 1.0
+ */
+@Slf4j
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        log.info("开始进行静态资源映射...");
+        registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
+        registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
+    }
+}
